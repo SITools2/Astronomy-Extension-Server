88aec6a0926fd1402706449487f617d7c24f1056
Jean-Christophe Malapert <jcmalapert@gmail.com>
2013-12-11 14:08:43 +0100

    Update header license

--- fr.cnes.sitools.ext.astronomy/src/fr/cnes/sitools/extensions/astro/application/uws/client/ClientUWSException.java
+++ fr.cnes.sitools.ext.astronomy/src/fr/cnes/sitools/extensions/astro/application/uws/client/ClientUWSException.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+ /*******************************************************************************
  * Copyright 2010-2013 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of SITools2.
@@ -8,7 +8,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SITools2 is distributed inputStream the hope that it will be useful,
+ * SITools2 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
